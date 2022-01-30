2f6df03e9d02e9bad679a7f1fd753aaf88f99256
aufexe <98711774+aufexe@users.noreply.github.com>
2022-01-30 22:31:54 +0200

    Add files via upload

---
+++ helper.lua
@@ -0,0 +1,122 @@
+local ini = require 'inicfg'
+local imgui = require 'imgui'
+local ini = require 'inicfg'
+local themes = import 'resource/imgui_themes.lua'
+
+local update_url = 'config/settings.ini'
+
+local dlstatus = require('moonloader').download_status
+update_state = false -- Если переменная == true, значит начнётся обновление.
+update_found = false -- Если будет true, будет доступна команда /update.
+
+local script_vers = 1.0
+local script_vers_text = "v1.0" -- Название нашей версии. В будущем будем её выводить ползователю.
+
+local update_url = '' -- Путь к ini файлу. Позже нам понадобиться.
+local update_path = getWorkingDirectory() .. "/update.ini"
+
+local script_url = '' -- Путь скрипту.
+local script_path = thisScript().path
+
+require 'lib.moonloader'
+local sampev = require 'lib.samp.events'
+
+require 'lib.moonloader'
+
+local x, y = getScreenResolution()
+
+local main_window_state = imgui.ImBool(false)
+
+function main()
+    while not isSampAvailable() do wait(0) end
+        imgui.SwitchContext()
+        sampRegisterChatCommand('helper', cmd_helper)
+    while true do
+        wait(0)
+        if not main_window_state.v then
+            imgui.Process = false
+        end
+    end
+end
+
+function apply_custom_style()
+    imgui.SwitchContext()
+    local style = imgui.GetStyle()
+    local colors = style.Colors
+    local clr = imgui.Col
+    local ImVec4 = imgui.ImVec4
+    style.WindowPadding = imgui.ImVec2(9, 5)
+    style.WindowRounding = 10
+    style.ChildWindowRounding = 10
+    style.FramePadding = imgui.ImVec2(5, 3)
+    style.FrameRounding = 6.0
+    style.ItemSpacing = imgui.ImVec2(9.0, 3.0)
+    style.ItemInnerSpacing = imgui.ImVec2(9.0, 3.0)
+    style.IndentSpacing = 21
+    style.ScrollbarSize = 6.0
+    style.ScrollbarRounding = 13
+    style.GrabMinSize = 17.0
+    style.GrabRounding = 16.0
+    style.WindowTitleAlign = imgui.ImVec2(0.5, 0.5)
+    style.ButtonTextAlign = imgui.ImVec2(0.5, 0.5)
+
+
+    colors[clr.Text]                   = ImVec4(0.90, 0.90, 0.90, 1.00)
+    colors[clr.TextDisabled]           = ImVec4(1.00, 1.00, 1.00, 1.00)
+    colors[clr.WindowBg]               = ImVec4(0.00, 0.00, 0.00, 1.00)
+    colors[clr.ChildWindowBg]          = ImVec4(0.00, 0.00, 0.00, 1.00)
+    colors[clr.PopupBg]                = ImVec4(0.00, 0.00, 0.00, 1.00)
+    colors[clr.Border]                 = ImVec4(0.82, 0.77, 0.78, 1.00)
+    colors[clr.BorderShadow]           = ImVec4(0.35, 0.35, 0.35, 0.66)
+    colors[clr.FrameBg]                = ImVec4(1.00, 1.00, 1.00, 0.28)
+    colors[clr.FrameBgHovered]         = ImVec4(0.68, 0.68, 0.68, 0.67)
+    colors[clr.FrameBgActive]          = ImVec4(0.79, 0.73, 0.73, 0.62)
+    colors[clr.TitleBg]                = ImVec4(0.00, 0.00, 0.00, 1.00)
+    colors[clr.TitleBgActive]          = ImVec4(0.46, 0.46, 0.46, 1.00)
+    colors[clr.TitleBgCollapsed]       = ImVec4(0.00, 0.00, 0.00, 1.00)
+    colors[clr.MenuBarBg]              = ImVec4(0.00, 0.00, 0.00, 0.80)
+    colors[clr.ScrollbarBg]            = ImVec4(0.00, 0.00, 0.00, 0.60)
+    colors[clr.ScrollbarGrab]          = ImVec4(1.00, 1.00, 1.00, 0.87)
+    colors[clr.ScrollbarGrabHovered]   = ImVec4(1.00, 1.00, 1.00, 0.79)
+    colors[clr.ScrollbarGrabActive]    = ImVec4(0.80, 0.50, 0.50, 0.40)
+    colors[clr.ComboBg]                = ImVec4(0.24, 0.24, 0.24, 0.99)
+    colors[clr.CheckMark]              = ImVec4(0.99, 0.99, 0.99, 0.52)
+    colors[clr.SliderGrab]             = ImVec4(1.00, 1.00, 1.00, 0.42)
+    colors[clr.SliderGrabActive]       = ImVec4(0.76, 0.76, 0.76, 1.00)
+    colors[clr.Button]                 = ImVec4(0.51, 0.51, 0.51, 0.60)
+    colors[clr.ButtonHovered]          = ImVec4(0.68, 0.68, 0.68, 1.00)
+    colors[clr.ButtonActive]           = ImVec4(0.67, 0.67, 0.67, 1.00)
+    colors[clr.Header]                 = ImVec4(0.72, 0.72, 0.72, 0.54)
+    colors[clr.HeaderHovered]          = ImVec4(0.92, 0.92, 0.95, 0.77)
+    colors[clr.HeaderActive]           = ImVec4(0.82, 0.82, 0.82, 0.80)
+    colors[clr.Separator]              = ImVec4(0.73, 0.73, 0.73, 1.00)
+    colors[clr.SeparatorHovered]       = ImVec4(0.81, 0.81, 0.81, 1.00)
+    colors[clr.SeparatorActive]        = ImVec4(0.74, 0.74, 0.74, 1.00)
+    colors[clr.ResizeGrip]             = ImVec4(0.80, 0.80, 0.80, 0.30)
+    colors[clr.ResizeGripHovered]      = ImVec4(0.95, 0.95, 0.95, 0.60)
+    colors[clr.ResizeGripActive]       = ImVec4(1.00, 1.00, 1.00, 0.90)
+    colors[clr.CloseButton]            = ImVec4(0.45, 0.45, 0.45, 0.50)
+    colors[clr.CloseButtonHovered]     = ImVec4(0.70, 0.70, 0.90, 0.60)
+    colors[clr.CloseButtonActive]      = ImVec4(0.70, 0.70, 0.70, 1.00)
+    colors[clr.PlotLines]              = ImVec4(1.00, 1.00, 1.00, 1.00)
+    colors[clr.PlotLinesHovered]       = ImVec4(1.00, 1.00, 1.00, 1.00)
+    colors[clr.PlotHistogram]          = ImVec4(1.00, 1.00, 1.00, 1.00)
+    colors[clr.PlotHistogramHovered]   = ImVec4(1.00, 1.00, 1.00, 1.00)
+    colors[clr.TextSelectedBg]         = ImVec4(1.00, 1.00, 1.00, 0.35)
+    colors[clr.ModalWindowDarkening]   = ImVec4(0.88, 0.88, 0.88, 0.35)
+end
+apply_custom_style()
+
+function imgui.OnDrawFrame()
+    local CorX, CorY, CorZ = getCharCoordinates(PLAYER_PED)
+    imgui.SetNextWindowPos(imgui.ImVec2(x / 2, y / 2), imgui.Cond.FirstUseEver, imgui.ImVec2(0.5, 0.5))
+    imgui.SetNextWindowSize(imgui.ImVec2(500, 300), imgui.Cond.FirstUseEver)
+    imgui.Begin('Helper', main_window_state)
+        imgui.Text('Данька твои корды: X:'..CorX..' Y:'..CorY..' Z:'..CorZ)
+    imgui.End()
+end
+
+function cmd_helper()
+    main_window_state.v = not main_window_state.v
+    imgui.Process = main_window_state.v
+end
